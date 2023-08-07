18b5c282f4e7e7fe5af876dd6471b6f3724de58e
https://github.com/CGVARGAS <claudio.g.vargas@gmail.com>
2023-08-07 11:40:49 -0300

    Criada a subclasse de Product e implementação

---
+++ program/Program.java
@@ -0,0 +1,20 @@
+package program;
+
+import java.util.*;
+
+public class Program{
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+
+
+        sc.close();
+
+    
+}
+
+}
+
+
+    
